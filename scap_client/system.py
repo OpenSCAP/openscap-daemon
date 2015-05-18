2733757695097481b090b58d7fc21c7718917e78
Martin Preisler <mpreisle@redhat.com>
2015-05-18 17:34:16 +0200

    Refuse to remove tasks with results stored

--- scap_client/system.py
+++ scap_client/system.py
@@ -217,6 +217,14 @@ class System(object):
                     (task_id)
                 )
 
+            result_ids = task.list_result_ids(self.results_dir)
+            if len(result_ids) > 0:
+                raise RuntimeError(
+                    "Can't remove task '%i', in has %i results stored. "
+                    "Please remove all the results first." %
+                    (task_id, len(result_ids))
+                )
+
             del self.tasks[task_id]
 
         os.remove(os.path.join(self.tasks_dir, "%i.xml" % (task_id)))
