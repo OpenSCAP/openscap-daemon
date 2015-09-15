c01d1dd48490d0866dc5ce85aee679f74f5a9610
Martin Preisler <mpreisle@redhat.com>
2015-09-15 16:28:43 +0200

    Added __init__.py to cve_scanner
    This fixes installation, sdist and other things related to setup.py

---
+++ openscap_daemon/cve_scanner/__init__.py
@@ -0,0 +1,18 @@
+# Copyright 2015 Red Hat Inc., Durham, North Carolina.
+# All Rights Reserved.
+#
+# openscap-daemon is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# openscap-daemon is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with openscap-daemon.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#__all__ = []
