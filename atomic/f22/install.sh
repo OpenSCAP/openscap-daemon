1e49df07f7c566e1f5e51333518dadd6a041e59b
Martin Preisler <mpreisle@redhat.com>
2015-10-07 16:06:50 +0200

    Further cleanup in F22 Dockerfile, changed install.sh

--- atomic/f22/install.sh
+++ atomic/f22/install.sh
@@ -1,6 +1,4 @@
 set -x
 
 echo "Adding the dbus configuration for the openscap-daemon to the host"
-
-cp -v /etc/dbus-1/system.d/org.openscapd.conf /host/etc/dbus-1/system.d/
-
+cp -v /etc/dbus-1/system.d/org.oscapd.conf /host/etc/dbus-1/system.d/
