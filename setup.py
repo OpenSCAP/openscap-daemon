dbfb65efde0416fbc3187be591bad08b361ad560
Martin Preisler <mpreisle@redhat.com>
2015-10-07 13:02:29 +0200

    Added a systemd unit file, modified setup.py to install it

--- setup.py
+++ setup.py
@@ -53,6 +53,9 @@ setup(
         os.path.join("bin", "oscapd-cli")
     ],
     data_files=[
-        (os.path.join("/", "etc", "dbus-1", "system.d"), ["org.openscapd.conf"])
+        (os.path.join("/", "etc", "dbus-1", "system.d"),
+         ["org.openscapd.conf"]),
+        (os.path.join("/", "usr", "lib", "systemd", "system"),
+         ["oscapd.service"]),
     ]
 )
