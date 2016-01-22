f2b4d144d0d8277e6676a2ef206fec8f334ff7fa
Martin Preisler <mpreisle@redhat.com>
2016-01-22 20:00:03 +0100

    Changed the CLI standalone integration test to catch problems with ./oscapd-cli

--- tests/integration/test_cli_standalone.sh
+++ tests/integration/test_cli_standalone.sh
@@ -2,7 +2,8 @@
 
 # calls in this test should not require oscapd to be running
 
-$PYTHON $BIN/oscapd-cli && exit 1
+$PYTHON $BIN/oscapd-cli
+[ $? -eq 2 ] || exit 1
 $PYTHON $BIN/oscapd-cli --version || exit 1
 $PYTHON $BIN/oscapd-cli -v || exit 1
 $PYTHON $BIN/oscapd-cli --help || exit 1
