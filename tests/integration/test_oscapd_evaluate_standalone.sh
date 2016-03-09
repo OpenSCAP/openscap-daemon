b872e4ea164737a26535691d9a12330851574c6b
Martin Preisler <mpreisle@redhat.com>
2016-03-09 16:58:57 -0500

    More tests for oscapd-evaluate

--- tests/integration/test_oscapd_evaluate_standalone.sh
+++ tests/integration/test_oscapd_evaluate_standalone.sh
@@ -2,5 +2,10 @@
 
 # calls in this test should not require oscapd to be running
 
+$PYTHON $BIN/oscapd-evaluate --help || exit 1
+$PYTHON $BIN/oscapd-evaluate --h || exit 1
+$PYTHON $BIN/oscapd-evaluate -v || exit 1
+$PYTHON $BIN/oscapd-evaluate config || exit 1
+$PYTHON $BIN/oscapd-evaluate --verbose config || exit 1
 $PYTHON $BIN/oscapd-evaluate spec --input ../testing_data/ssg-fedora-ds.xml --print-xml || exit 1
 $PYTHON $BIN/oscapd-evaluate spec --input ../testing_data/ssg-fedora-ds.xml --profile xccdf_org.ssgproject.content_profile_common --print-xml || exit 1
