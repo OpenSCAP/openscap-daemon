13113103eeaeb4c3c522399ed0ac9056c71c8af6
Martin Preisler <mpreisle@redhat.com>
2016-03-24 16:27:53 -0400

    Fixed FSF address in cve_scanner subpackage

--- openscap_daemon/cve_scanner/reporter.py
+++ openscap_daemon/cve_scanner/reporter.py
@@ -11,10 +11,8 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
+# You should have received a copy of the GNU Lesser General Public License
+# along with openscap-daemon.  If not, see <http://www.gnu.org/licenses/>.
 
 '''Reporter Class'''
 
@@ -31,7 +29,7 @@ class Reporter(object):
         self.ac = appc
         self.report_dir = os.path.join(self.ac.reportdir, "reports")
         self.ac.docker_state = os.path.join(self.report_dir,
-                                              "docker_state.json")
+                                            "docker_state.json")
 
         if not os.path.exists(self.report_dir):
             os.mkdir(self.report_dir)
