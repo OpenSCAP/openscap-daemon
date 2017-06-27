9c135801e7afaa131065f7eff05b6d886da90a0e
Jan Černý <jcerny@redhat.com>
2017-06-27 11:33:57 +0200

    Add a script for generating Dockerfile and build environment
    The ./generate-dockerfile.py can be used for generating
    Dockerfiles that will contain OpenSCAP, SCAP Security
    Guide and OpenSCAP daemon either from packages, or from
    upstream sources on Github.

---
+++ container/generate-dockerfile.py
@@ -0,0 +1,164 @@
+#!/usr/bin/python
+
+import argparse
+
+labels = [
+    ("com.redhat.component", "openscap-docker"),
+    ("name", "openscap"),
+    ("version", "testing"),
+    ("architecture", "x86_64"),
+    ("summary", "OpenSCAP container image that provides security/compliance scanning capabilities for 'atomic scan'"),
+    ("description", "OpenSCAP is an auditing tool that utilizes the Extensible Configuration Checklist Description Format (XCCDF). XCCDF is a standard way of expressing checklist content and defines security checklists."),
+    ("io.k8s.display-name", "OpenSCAP"),
+    ("io.k8s.description", "OpenSCAP is an auditing tool that utilizes the Extensible Configuration Checklist Description Format (XCCDF). XCCDF is a standard way of expressing checklist content and defines security checklists."),
+    ("io.openshift.tags", "security openscap scan"),
+    ("install", "docker run --rm --privileged -v /:/host/ IMAGE sh /root/install.sh"),
+    ("run", "docker run -it --rm -v /:/host/ IMAGE sh /root/run.sh")
+]
+packages = [
+    "bzip2",
+    "wget"
+]
+files = [
+    ("install.sh", "/root"),
+    ("run.sh", "/root"),
+    ("openscap", "/root"),
+    ("config.ini", "/root")
+]
+env_variables = [
+    ("container", "docker")
+]
+install_commands = {
+    "fedora": "dnf",
+    "rhel": "yum"
+}
+builddep_packages = {
+    "fedora" : "'dnf-command(builddep)'",
+    "rhel" : "yum-utils"
+}
+builddep_commands = {
+    "fedora": "dnf -y builddep",
+    "rhel": "yum-builddep -y"
+}
+download_cve_feeds_command = [
+    "wget --no-verbose -P /var/lib/oscapd/cve_feeds/ "
+    "https://www.redhat.com/security/data/oval/com.redhat.rhsa-RHEL{5,6,7}.xml.bz2",
+    "bzip2 -dk /var/lib/oscapd/cve_feeds/com.redhat.rhsa-RHEL{5,6,7}.xml.bz2",
+    "ln -s /var/lib/oscapd/cve_feeds/ /var/tmp/image-scanner"
+]
+openscap_build_command = [
+    "git clone -b maint-1.2 https://github.com/OpenSCAP/openscap.git",
+    "pushd /openscap",
+    "./autogen.sh",
+    "./configure --enable-sce --prefix=/usr",
+    "make -j 4 install",
+    "popd"
+]
+ssg_build_command = [
+    "git clone https://github.com/OpenSCAP/scap-security-guide.git",
+    "pushd /scap-security-guide/build",
+    "cmake -DCMAKE_INSTALL_DATADIR=/usr/share ..",
+    "make -j 4 install",
+    "popd"
+]
+daemon_build_command = [
+    "git clone https://github.com/OpenSCAP/openscap-daemon.git",
+    "pushd /openscap-daemon",
+    "python setup.py install",
+    "popd"
+]
+delim = " && \\\n    "
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Builds an image with OpenSCAP Daemon")
+    parser.add_argument("--base", type=str, default="fedora", help="Base image name")
+    parser.add_argument("--openscap-from-git", action="store_true", default=False,
+                        help="Use OpenSCAP from upstream instead of package")
+    parser.add_argument("--ssg-from-git", action="store_true", default=False,
+                        help="Use SCAP Security Guide from upstream instead of package")
+    parser.add_argument("--daemon-from-git", action="store_true", default=False,
+                        help="Use OpenSCAP Daemon from upstream instead of package")
+    args = parser.parse_args()
+
+    f = open("Dockerfile", "w")
+
+    install_command = install_commands.get(args.base, install_commands["rhel"])
+    builddep_package = builddep_packages.get(args.base, builddep_packages["rhel"])
+    builddep_command = builddep_commands.get(args.base, builddep_commands["rhel"])
+
+    # write out the Dockerfile
+    f.write("FROM " + args.base + "\n\n")
+
+    # add labels
+    for name, value in labels:
+        f.write("LABEL " + name + '="' + value + '"\n')
+    f.write("\n")
+
+    # add environment variables
+    for var, val in env_variables:
+        f.write("ENV " + var + " " + val + "\n")
+    f.write("\n")
+
+    build_from_source = []
+    build_commands = []
+
+    # OpenSCAP
+    if args.openscap_from_git:
+        packages.extend(["git", "libtool", "automake"])
+        build_from_source.append("openscap")
+        build_commands.append(openscap_build_command)
+    else:
+        packages.append("openscap-utils")
+
+    # SCAP Security Guide
+    if args.ssg_from_git:
+        packages.append("git")
+        build_from_source.append("scap-security-guide")
+        build_commands.append(ssg_build_command)
+    else:
+        packages.append("scap-security-guide")
+
+    # OpenSCAP Daemon
+    if args.daemon_from_git:
+        packages.append("git")
+        build_from_source.append("openscap-daemon")
+        build_commands.append(daemon_build_command)
+    else:
+        packages.append("openscap-daemon")
+
+    # inject files
+    for file, path in files:
+        f.write("ADD " + file + " " + path + "\n")
+    f.write("\n")
+
+    if build_from_source:
+        packages.append(builddep_package)
+
+    # add a command to install packages
+    f.write("RUN " + install_command + " -y install " + " ".join(set(packages)) + "\n\n")
+
+    if build_from_source:
+        # install build dependencies
+        f.write("RUN " + builddep_command + " " + " ".join(build_from_source) + "\n\n")
+
+    # clean package manager cache
+    f.write("RUN " + install_command + " clean all\n\n")
+
+    if build_from_source:
+        # add commands for building from custom sources
+        for cmd in build_commands:
+            f.write("RUN " + delim.join(cmd) + "\n\n")
+
+    # add RUN instruction that will download CVE feeds
+    f.write("RUN " + delim.join(download_cve_feeds_command) + "\n\n")
+
+    # add CMD instruction to the Dockerfile, including a comment
+    f.write("# It doesn't matter what is in the line below, atomic will change the CMD\n")
+    f.write("# before running it\n")
+    f.write('CMD ["/root/run.sh"]\n')
+
+    f.close()
+
+if __name__ == "__main__":
+    main()
