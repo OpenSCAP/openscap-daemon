fe98965cb00c5a14ecaf8f0f0039b9befdfd0688
Matus Marhefka <mmarhefk@redhat.com>
2018-01-17 05:08:04 -0500

    Added tests to cover RHBZ#1379083

--- tests/integration/test_oscapd_cli_standalone.sh
+++ tests/integration/test_oscapd_cli_standalone.sh
@@ -8,3 +8,11 @@ $PYTHON $BIN/oscapd-cli --version || exit 1
 $PYTHON $BIN/oscapd-cli -v || exit 1
 $PYTHON $BIN/oscapd-cli --help || exit 1
 $PYTHON $BIN/oscapd-cli -h || exit 1
+
+useradd testuser
+out=$(su testuser -c "$PYTHON $BIN/oscapd-cli status 2>&1")
+rv=$?
+userdel -r testuser
+[ $rv -ne 0 ] || exit 1
+grep -iq "traceback" <<< "$out"
+[ $? -ne 0 ] || exit 1
--- tests/integration/test_oscapd_evaluate_standalone.sh
+++ tests/integration/test_oscapd_evaluate_standalone.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # calls in this test should not require oscapd to be running
-
 $PYTHON $BIN/oscapd-evaluate --help || exit 1
 $PYTHON $BIN/oscapd-evaluate --h || exit 1
 $PYTHON $BIN/oscapd-evaluate -v || exit 1
@@ -9,3 +8,19 @@ $PYTHON $BIN/oscapd-evaluate config || exit 1
 $PYTHON $BIN/oscapd-evaluate --verbose config || exit 1
 $PYTHON $BIN/oscapd-evaluate spec --input ../testing_data/ssg-fedora-ds.xml --print-xml || exit 1
 $PYTHON $BIN/oscapd-evaluate spec --input ../testing_data/ssg-fedora-ds.xml --profile xccdf_org.ssgproject.content_profile_common --print-xml || exit 1
+
+
+useradd testuser
+out=$(su testuser -c "$PYTHON $BIN/oscapd 2>&1")
+rv=$?
+out2=$(su testuser -c "$PYTHON $BIN/oscapd-evaluate 2>&1")
+rv2=$?
+userdel -r testuser
+
+[ $rv -ne 0 ] || exit 1
+grep -iq "traceback" <<< "$out"
+[ $? -ne 0 ] || exit 1
+
+[ $rv2 -ne 0 ] || exit 1
+grep -iq "traceback" <<< "$out2"
+[ $? -ne 0 ] || exit 1
